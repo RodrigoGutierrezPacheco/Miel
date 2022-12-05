ac27d8daedddcc879f3ed24287b22244bccfda6c
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-12-05 14:31:36 -0600

    primer commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">La Miel de Jaimes🐝</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Inicio</Nav.Link>
+            <Nav.Link href="#pricing">¿Quienes somos?</Nav.Link>
+            <NavDropdown title="Productos" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Miel</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Dulces
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Jabon</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Shampoo
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Pedidos</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Contacto
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import "./HomePage.css";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Miel del Primo Jaimes🐝</h1>
+    </div>
+  );
+}
+
+export default HomePage;
